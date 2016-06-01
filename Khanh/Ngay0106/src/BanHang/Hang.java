d308a91b9e7fef7d19f58216509d0427e536d82a
KhanhMata <radnvk@gmail.com>
2016-06-01 16:40:34 +0700

    Ngay quoc te tre trau
    Hang - Chi nhanh - Nhan vien

---
+++ Khanh/Ngay0106/src/BanHang/Hang.java
@@ -0,0 +1,40 @@
+package BanHang;
+
+public class Hang {
+	private int idHang;
+	private int idNvien;
+	private String tenHang;
+	private int soluong;
+
+	public int getIdHang() {
+		return idHang;
+	}
+
+	public void setIdHang(int idHang) {
+		this.idHang = idHang;
+	}
+
+	public int getIdNvien() {
+		return idNvien;
+	}
+
+	public void setIdNvien(int idNvien) {
+		this.idNvien = idNvien;
+	}
+
+	public String getTenHang() {
+		return tenHang;
+	}
+
+	public void setTenHang(String tenHang) {
+		this.tenHang = tenHang;
+	}
+
+	public int getSoLuong() {
+		return soluong;
+	}
+
+	public void setSoLuong(int soLuong) {
+		this.soluong = soLuong;
+	}
+}
---
+++ Khanh/Ngay0106/src/BanHang/NhanVien.java
@@ -0,0 +1,46 @@
+package BanHang;
+
+public class NhanVien {
+	private int idNhanvien;
+	private int idNhanh;
+	private String tenNhanvien;
+	private int tuoi;
+	private String gioitinh;
+	private String diachi;
+	public int getIdNhanvien() {
+		return idNhanvien;
+	}
+	public void setIdNhanvien(int idNhanvien) {
+		this.idNhanvien = idNhanvien;
+	}
+	public int getIdNhanh() {
+		return idNhanh;
+	}
+	public void setIdNhanh(int idNhanh) {
+		this.idNhanh = idNhanh;
+	}
+	public String getTenNhanvien() {
+		return tenNhanvien;
+	}
+	public void setTenNhanvien(String tenNhanvien) {
+		this.tenNhanvien = tenNhanvien;
+	}
+	public int getTuoi() {
+		return tuoi;
+	}
+	public void setTuoi(int tuoi) {
+		this.tuoi = tuoi;
+	}
+	public String getGioitinh() {
+		return gioitinh;
+	}
+	public void setGioitinh(String gioitinh) {
+		this.gioitinh = gioitinh;
+	}
+	public String getDiachi() {
+		return diachi;
+	}
+	public void setDiachi(String diachi) {
+		this.diachi = diachi;
+	}
+}
